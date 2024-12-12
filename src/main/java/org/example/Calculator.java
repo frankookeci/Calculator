6ea811f05dc771211f50a795509fad01ba1586ec
frankookeci <frankokeci06@gmail.com>
2024-12-12 11:53:30 +0100

    Added Multiply Methode

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -8,6 +8,9 @@ public class Calculator {
     public int multiply(int a, int b) {
         return a * b;
     }
-    
 
+    public int divide(int a, int b) {
+        if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
+        return a / b;
+    }
     }
