12a7cdc6867b192508f35272e072b02fd393b77f
frankookeci <frankokeci06@gmail.com>
2024-12-12 11:54:44 +0100

    Added Subtract Methode

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -9,6 +9,10 @@ public class Calculator {
         return a * b;
     }
 
+    public int subtract(int a, int b) {
+        return a * b;
+    }
+
     public int dividieren(int a, int b) {
         if (b == 0) throw new IllegalArgumentException("Cannot divide by zero");
         return a / b;
